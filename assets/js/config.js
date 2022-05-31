a80582f364782e87264c5ff78ee41967e0194a7c
summonhalfa <haddadilyas02@gmail.com>
2022-05-31 15:33:09 +0200

    Versione 1.0

---
+++ assets/js/config.js
@@ -0,0 +1,11 @@
+const API_KEY = "a0fd19d4";
+export const BASE_URL = `https://www.omdbapi.com/?apikey=${API_KEY}&`
+
+
+export const CONTENT_TYPE = {
+    MOVIE: 'movie',
+    SERIES: "series" ,
+    EPISODE: "episode",
+    GAME: "game"
+}
+
